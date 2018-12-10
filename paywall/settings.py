0734f6cf4f1ac24a0a5803452b6170b4e7bbf988
mohan3d <mohan3d94@gmail.com>
2018-12-10 11:33:36 +0200

    feat: add wall app (built reactJS app)

--- paywall/settings.py
+++ paywall/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.apps.PostsConfig'
+    'posts.apps.PostsConfig',
+    'wall.apps.WallConfig',
 ]
 
 MIDDLEWARE = [
@@ -59,8 +60,7 @@ ROOT_URLCONF = 'paywall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- paywall/urls.py
+++ paywall/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('wall.urls')),
     path('api/v1/', include('api.rest.v1.urls')),
     path('admin/', admin.site.urls),
 ]
